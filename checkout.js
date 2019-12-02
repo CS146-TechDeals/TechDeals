3e1c069cb6e825e013e24ab71cbf79907f4938b1
iFallenGuns <55553224+iFallenGuns@users.noreply.github.com>
2019-12-01 22:55:31 -0500

    quick fix

--- checkout.js
+++ checkout.js
@@ -1,15 +1,17 @@
 function loadCart(){
 
     var temp = document.getElementById("TAB");
+    if(localStorage.length != 0){
+        for (var i = 0; i < localStorage.length; i++){
+            // create new row and 2 cells for item and price
+            var newRow = temp.insertRow(temp.length);
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
 
-    for (var i = 0; i < localStorage.length; i++){
-        // create new row and 2 cells for item and price
-        var newRow = temp.insertRow(temp.length);
-        var cell1 = newRow.insertCell(0);
-        var cell2 = newRow.insertCell(1);
-
-        //fill cells
-        cell1.innerHTML = localStorage.key(i);
-        cell2.innerHTML = localStorage[localStorage.key(i)];
+            //fill cells
+            cell1.innerHTML = localStorage.key(i);
+            cell2.innerHTML = localStorage[localStorage.key(i)];
+        }
     }
+    
 }
